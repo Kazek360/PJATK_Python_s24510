a9496b1bb25fb096e3a04e2183f7d52e4123cc30
Kazek360 <96256193+Kazek360@users.noreply.github.com>
2023-05-17 11:28:03 +0200

    Create main.py

---
+++ PPY10/Zad9/main.py
@@ -0,0 +1,8 @@
+import geographicalUnits
+
+print(geographicalUnits.kilometersToMiles(10))
+print(geographicalUnits.milesToKilometers(10))
+print(geographicalUnits.feetToMeters(10))
+print(geographicalUnits.metersToFeets(10))
+print(geographicalUnits.degreesToRadians(10))
+print(geographicalUnits.radiansToDegrees(10))
